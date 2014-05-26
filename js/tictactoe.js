51060e071735d04709f84b9923124f6816514c92
DachiCoding <pwpnx360@gmail.com>
2014-05-26 11:18:58 -0500

    tictactoe update 20140526

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,16 +2,21 @@
 //Dachi Xu, dachi.xu@gmail.com
 //Thanks to http://jsfiddle.net/rtoal/5wKfF/ for the game logic
 
-//INITIALIZE GLOBAL VARIABLES
+//***********************************************************************
+//*****************INITIALIZE GLOBAL VARIABLES***************************
 var gameTurn = 0;
 var ifRest = false;
 var scores = {"X":0,"O":0};
 var gameStatus = "on"; //Player can move only gameStatus is "on", and the cubic should be blinking.
+
+/*
 var ifTaken = { 
 		"c1":0,"c2":0,"c3":0, //if the single cubic is available
 		"c4":0,"c5":0,"c6":0,
 		"c7":0,"c8":0,"c9":0 
 	      };
+*/
+
 var HUD;
 var cubicVal = {
 		"c1":1,"c2":2,"c3":4,
@@ -20,8 +25,98 @@ var cubicVal = {
 	       };
 var winSet = [7,56,448,73,146,292,273,84];
 
-window.onload = function(){
- //***********************************************************************
+//**************************************************************
+//*********************MOVE FUNCTIONs***************************
+function playerMove(){
+  while (gameStatus === "on"){
+   this.innerHTML = "X";
+   gameTurn++;
+   gameStatus = "off";
+   HUD.innerHTML = "A.I's Move";
+   scores["X"] += cubicVal[this.id];
+  }
+}
+
+function aiMove(){
+   this.innerHTML = "O";
+   gameTurn++;
+   gameStatus = "on";
+   HUD.innerHTML = "Your Move";
+   scores["O"] += cubicVal[this.id];
+}
+
+//**************************************************************
+//*********************NEW GAME FUNCTION**********************
+function newGame(){
+
+ //clean all cubics
+ $(".chartunit").removeClass("cubicSpin");
+ setTimeout(function(){$(".chartunit").addClass('cubicSpin')},0);
+ var cubics = document.getElementsByClassName("chartunit");
+ for(var i = 0; i < cubics.length; i++){
+   cubics[i].innerHTML = "";
+ }
+
+ //clean all notification
+ $("#winDiv").fadeOut(500);
+ $("#drawDiv").fadeOut(500);
+ $("#loseDiv").fadeOut(500);
+
+ //reset the gameflow
+ gameTurn = 0;
+ scores["X"] = 0;
+ scores["O"] = 0;
+ gameStatus = "on";
+
+ //reset newGame
+ HUD.innerHTML = "Your Move";
+}
+
+//**************************************************************
+//*********************IF WIN FUNCTION**************************
+function ifWin(role){
+ for (var i = 0; i <= winSet.length; i++){
+  if ((winSet[i] & scores[role]) === winSet[i]){
+   return true;
+  }
+ }
+}
+
+//**************************************************************
+//*********************CHECK FUNCTION***************************
+function check(){
+ var result;
+ if (ifWin("X")){
+  result = "win";
+  gameStatus = "off";
+  scores["X"] = 0;
+  scores["O"] = 0;
+ }
+ else if (ifWin("O")) {
+  result = "lose";
+  gameStatus = "off";
+  scores["X"] = 0;
+  scores["O"] = 0;
+ }
+ else if ( !ifWin("X") && !ifWin("O") && gameTurn === 9){
+  result = "draw";
+  gameStatus = "off";
+  scores["X"] = 0;
+  scores["O"] = 0;
+ }
+
+ if (result === "win"){
+   $("#winDiv").fadeIn(500);
+ } else if (result === "draw"){
+   $("#drawDiv").fadeIn(500);
+ } else if (result === "lose"){
+   $("#loseDiv").fadeIn(500);
+ }
+}
+
+//***********************************************************************
+//*******************GAME FLOW CONTROLLER********************************
+window.onload = function() {
  //INITIALIZE ENVIORMENT AND SETUP
  //Get the elements
  var cubic1 = document.getElementById("c1");
@@ -41,80 +136,26 @@ window.onload = function(){
  HUD = document.getElementById("HUD");
  HUD.innerHTML = "Your Move";
 
- //***********************************************************************
  //START GAME FLOW
- cubic1.addEventListener('click',Move,false);
- cubic2.addEventListener('click',Move,false);
- cubic3.addEventListener('click',Move,false);
- cubic4.addEventListener('click',Move,false);
- cubic5.addEventListener('click',Move,false);
- cubic6.addEventListener('click',Move,false);
- cubic7.addEventListener('click',Move,false);
- cubic8.addEventListener('click',Move,false);
- cubic9.addEventListener('click',Move,false);
- 
-}
+ //Make all cubic available for the first turn
+ cubic1.addEventListener('click',playerMove,false);
+ cubic2.addEventListener('click',playerMove,false);
+ cubic3.addEventListener('click',playerMove,false);
+ cubic4.addEventListener('click',playerMove,false);
+ cubic5.addEventListener('click',playerMove,false);
+ cubic6.addEventListener('click',playerMove,false);
+ cubic7.addEventListener('click',playerMove,false);
+ cubic8.addEventListener('click',playerMove,false);
+ cubic9.addEventListener('click',playerMove,false);
 
-function Move(){
-  if (gameTurn % 2 === 0){
-   this.innerHTML = "X";
-   gameTurn++;
-   gameStatus = "off";
-   HUD.innerHTML = "A.I's Move";
-   scores["X"] += cubicVal[this.id];
-  } else {
-   this.innerHTML = "O";
-   gameTurn++;
-   gameStatus = "on";
-   HUD.innerHTML = "Your Move";
-   scores["O"] += cubicVal[this.id];
-  }
-  check();
-}
+ // -> If an empty cubic is clicked 
+ // -> Remove event listener from the cubic clicked
+ // -> Judge situation 
+ // -> A.I move
+ // -> Remove event listener from the cubic A.I occupied
+ // -> Wait for next input or reach a game result
 
-//Start A New Game
-function newGame(){
-
- //clean all cubics
- $(".chartunit").removeClass("cubicSpin");
- setTimeout(function(){$(".chartunit").addClass('cubicSpin')},0);
- var cubics = document.getElementsByClassName("chartunit");
- for(var i = 0; i < cubics.length; i++){
-   cubics[i].innerHTML = "";
- }
-
- //clean all notification
- $("#winDiv").fadeOut(500);
- $("#drawDiv").fadeOut(500);
- $("#loseDiv").fadeOut(500);
-
- //reset the gameflow
- gameTurn = 0;
-
- //reset newGame
- HUD.innerHTML = "Your Move";
-}
-
-function check(){
- var result;
- //check
- for (var i = 0; i < winSet.length; i++){
-  if ((winSet[i] & scores["X"]) === winSet[i]){
-   result = "win";
-  }
-  else if ((winSet[i] & scores["O"]) === winSet[i]){
-   result = "lose";
-  }
-  else if (gameTurn === 9){
-   result = "draw";
-  }
- }
- 
- if (result === "win"){
-   $("#winDiv").fadeIn(500);
- } else if (result === "draw"){
-   $("#drawDiv").fadeIn(500);
- } else if (result === "lose"){
-   $("#loseDiv").fadeIn(500);
- }
+ cubic2.moveFunc = aiMove; 
+ cubic2.moveFunc();
+ cubic2.removeEventListener('click',playerMove,false);
 }
